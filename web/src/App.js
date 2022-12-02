5ccdab49cdbd88356bc58b684a4fe9580b710d87
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-12-02 18:35:51 +0500

    code push

--- web/src/App.js
+++ web/src/App.js
@@ -4,30 +4,31 @@ import axios from "axios";
 import "./App.css";
 
 function Weather() {
-  const [weatherData, setWeatherData] = useState(null);
-  const [cityName, setCityName] = useState("");
+  // const [weatherData, setWeatherData] = useState(null);
+  // const [cityName, setCityName] = useState("");
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
 
     console.log("I am click handler");
     axios
       .get(
-        `https://express-api.up.railway.app/`
+        `http://express-api.up.railway.app/`
       )
       .then((response) => {
         console.log("response: ", response.data);
 
-        setWeatherData(response.data);
+        // setWeatherData(response.data);
+        console.log(response.data);
       })
       .catch((err) => {
         console.log("error: ", err);
       });
-  };
+  // };
 
   return (
     <div>
-      <form onSubmit={submitHandler}>
+      {/* <form onSubmit={submitHandler}>
         City Name:
         <input
           type="text"
@@ -38,7 +39,7 @@ function Weather() {
           }}
         />
         <button type="submit">get weather</button>
-      </form>
+      </form> */}
       <br />
       <br />
     </div>
