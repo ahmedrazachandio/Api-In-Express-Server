578a43aba8b4900c81d1ae988db8da89b6574423
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-12-02 18:32:13 +0500

    code push

--- server.mjs
+++ server.mjs
@@ -1,6 +1,8 @@
 // const express = require('express')
 console.log("i am server file");
-import express from 'express';
+import express from "express";
+import path from "path";
+import cors from "cors";
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,7 +10,22 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
     console.log("request ip:",req.ip);
   res.send('Hello World' + new Date().toDateString())
-})
+});
+app.get("/weather", (req, res) => {
+  // console.log("request ip:", req.ip);
+  res.send({
+    city: "Karachi",
+    text: "Clear",
+    tempC: 22,
+    tempF: 71,
+    ftempC: 20,
+    ftempF: 68,
+    humidity: 83,
+    visibility: "6KM",
+    windKPH: 6.8,
+    windMPH: 4.3,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ web/src/App.js
@@ -0,0 +1,48 @@
+import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function Weather() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [cityName, setCityName] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log("I am click handler");
+    axios
+      .get(
+        `https://express-api.up.railway.app/`
+      )
+      .then((response) => {
+        console.log("response: ", response.data);
+
+        setWeatherData(response.data);
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        City Name:
+        <input
+          type="text"
+          placeholder="enter your city name"
+          required
+          onChange={(e) => {
+            setCityName(e.target.value);
+          }}
+        />
+        <button type="submit">get weather</button>
+      </form>
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default Weather;
