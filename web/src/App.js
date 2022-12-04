0b736d6aebd715772ecbc8f3725383b869c7334d
ahmedrazachandio <ahmadraza.azt@gmail.com>
2022-12-05 01:52:10 +0500

    requirments complete

--- web/src/App.js
+++ web/src/App.js
@@ -1,39 +1,45 @@
 import logo from "./logo.svg";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function Weather() {
-  const [weatherData, setWeatherData] = useState(null);
-  // const [cityName, setCityName] = useState("");
+  // const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
+  const [cityName, setCityName] = useState("");
 
-  // const submitHandler = (e) => {
-  //   e.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    var inputValue = document.getElementById('cityName').value;
+    setCityName(inputValue)
+    console.log(inputValue);
 
-  // console.log("I am click handler");
+  console.log("I am click handler");
   axios.get(
-    `https://express-api.up.railway.app/weather`
+    `http://express-api.up.railway.app/weather`
   )
     .then((response) => {
-      // console.log("response: ", response);
+      console.log("response: ", response.data);
 
-      setWeatherData(response);
-      // console.log(weatherData);
+      setWeatherData(response.data);
+      console.log(weatherData);
     })
     .catch((err) => {
       console.log("error: ", err);
     });
-  // };
+  };
 
   return (
     <div>
       <nav className="nav-bar">
         <h1>Weather App</h1>
       </nav>
-      {/* <form onSubmit={submitHandler}>
+       <form onSubmit={submitHandler}> 
         City Name:
         <input
           type="text"
+          id="cityName"
+          // value=""
           placeholder="enter your city name"
           required
           onChange={(e) => {
@@ -41,7 +47,7 @@ function Weather() {
           }}
         />
         <button type="submit">get weather</button>
-      </form> */}
+      </form> 
       <br />
       <br />
       <div className="card">
@@ -53,28 +59,27 @@ function Weather() {
             {/* <img src="//cdn.weatherapi.com/weather/64x64/night/113.png" alt="" id="weather_icon" /> */}
           {/* </div>  */}
           <div className="temp_result">
-            <div id="tempC">weatherData?.tempC)°C</div>
-            <div id="tempF">{Math.round(weatherData?.tempF)}°F</div>
+            <div id="tempC">{weatherData?.ftempC}°C</div>
+            <div id="tempF">{Math.round(weatherData?.ftempF)}°F</div>
           </div>
 
         </div>
-
         <div className="sub">
           <div className="left">
             <div id="time">{new Date().toDateString()}</div>
-            {/* <div id="city_name">{weatherData.city}</div> */}
-            {/* <div id="feels_like_c">Feels Like:{weatherData.ftempC}°C</div> */}
-            {/* <div id="humidity">Humidity:{weatherData.humidity}%</div> */}
-            {/* <div id="wind_speed_kph">Wind Speed:{weatherData.windKPH}KP/H</div> */}
+            <div id="city_name">{cityName}</div>
+            <div id="feels_like_c">Feels Like:{weatherData.ftempC}°C</div>
+            <div id="humidity">Humidity:{weatherData.humidity}%</div>
+            <div id="wind_speed_kph">Wind Speed:{weatherData.windKPH}KP/H</div>
 
           </div>
 
           <div className="right">
             <div id="last_update">{new Date().toDateString()}Last Update</div>
-            {/* <div id="weather_condition">{weatherData.text}</div> */}
-            {/* <div id="feels_like_f">Feels Like:{weatherData.ftempC}°F</div> */}
-            {/* <div id="visibility">Visibility{weatherData.visibility}KM</div> */}
-            {/* <div id="wind_speed_mph">Wind Speed:{weatherData.windMPH}MP/H</div> */}
+            <div id="weather_condition">{weatherData.text}</div>
+            <div id="feels_like_f">Feels Like:{weatherData.ftempC}°F</div>
+            <div id="visibility">Visibility{weatherData.visibility}KM</div>
+            <div id="wind_speed_mph">Wind Speed:{weatherData.windMPH}MP/H</div>
           </div>
         </div>
 
